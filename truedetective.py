e314adf15ce8ea6af4595101d901ba5ed60b8742
Tisóczki Tamás <tamas.tisoczki@gmail.com>
2020-07-06 17:42:11 +0200

    is_sunday and should_bring_umbrella working

--- truedetective.py
+++ truedetective.py
@@ -12,11 +12,16 @@ def is_leap_year(year):
 
 
 def is_sunday(day, weekday_of_first):
-    pass
+    return True if (weekday_of_first == 'M' and day % 7 == 0) or (weekday_of_first == 'Tu' and (day + 1) % 7 == 0) \
+        or (weekday_of_first == 'W' and (day + 2) % 7 == 0) or (weekday_of_first == 'Th' and (day + 3) % 7 == 0) \
+        or (weekday_of_first == 'F' and (day + 4) % 7 == 0) or (weekday_of_first == "Sa" and (day + 5) % 7 == 0) \
+        or (weekday_of_first == "Su" and (day + 6) % 7 == 0) else False
 
 
 def should_bring_umbrella(rains, wind_scale, cloudy, red_sky, strong_flower_smell, spiders_down, lying_cattle, strong_sunshine):
-    pass
+    return True if (rains and (wind_scale < 7)) \
+                   or (cloudy and (wind_scale < 7) and (red_sky or strong_flower_smell or spiders_down or lying_cattle)) \
+                   or (strong_sunshine and (wind_scale < 7)) else False
 
 
 def should_take_a_nap(want_to, trouble_sleeping, after_4pm, at_work, mad_boss, have_30m, have_10m):
--- whatif.py
+++ whatif.py
@@ -29,11 +29,49 @@ def is_leap_year(year):
 
 
 def is_sunday(day, weekday_of_first):
-    pass
+    if weekday_of_first == 'M':
+        first_day = 0
+    elif weekday_of_first == 'Tu':
+        first_day = 1
+    elif weekday_of_first == 'W':
+        first_day = 2
+    elif weekday_of_first == 'Th':
+        first_day = 3
+    elif weekday_of_first == 'F':
+        first_day = 4
+    elif weekday_of_first == 'Sa':
+        first_day = 5
+    elif weekday_of_first == 'Su':
+        first_day = 6
+
+    if (day + first_day) % 7 == 0:
+        return True
+    else:
+        return False
 
 
 def should_bring_umbrella(rains, wind_scale, cloudy, red_sky, strong_flower_smell, spiders_down, lying_cattle, strong_sunshine):
-    pass
+    if rains:
+        if wind_scale < 7:
+            return True
+        return False
+    if cloudy:
+        if wind_scale < 7:
+            if red_sky:
+                return True
+            if strong_flower_smell:
+                return True
+            if spiders_down:
+                return True
+            if lying_cattle:
+                return True
+        return False
+    if strong_sunshine:
+        if wind_scale < 7:
+            return True
+        return False
+    else:
+        return False
 
 
 def should_take_a_nap(want_to, trouble_sleeping, after_4pm, at_work, mad_boss, have_30m, have_10m):
